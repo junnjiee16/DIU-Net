4d1f8dab51433dfdcb54d82b77e964d1d48bc867
junnjiee16 <junnjiee16@gmail.com>
2024-04-03 17:06:28 +0800

    add scaling factor to diunet model

--- diunet/diunet.py
+++ diunet/diunet.py
@@ -9,42 +9,64 @@ class DIUNet(nn.Module):
     PyTorch implementation of the Dense Inception U-Net (DIU-Net)
     """
 
-    def __init__(self, scale=1):
+    def __init__(self, out_channels: int, scale=1):
         super(DIUNet, self).__init__()
 
         # analysis path
-        self.analysis_inception1 = InceptionResBlock(in_channels=1, out_channels=64)
-        self.analysis_downsampling1 = DownsamplingBlock(in_channels=64)
-        self.analysis_inception2 = InceptionResBlock(in_channels=64, out_channels=128)
-        self.analysis_downsampling2 = DownsamplingBlock(in_channels=128)
-        self.analysis_inception3 = InceptionResBlock(in_channels=128, out_channels=256)
-        self.analysis_downsampling3 = DownsamplingBlock(in_channels=256)
+        self.analysis_inception1 = InceptionResBlock(
+            in_channels=1, out_channels=int(64 * scale)
+        )
+        self.analysis_downsampling1 = DownsamplingBlock(in_channels=int(64 * scale))
+        self.analysis_inception2 = InceptionResBlock(
+            in_channels=int(64 * scale), out_channels=int(128 * scale)
+        )
+        self.analysis_downsampling2 = DownsamplingBlock(in_channels=int(128 * scale))
+        self.analysis_inception3 = InceptionResBlock(
+            in_channels=int(128 * scale), out_channels=int(256 * scale)
+        )
+        self.analysis_downsampling3 = DownsamplingBlock(in_channels=int(256 * scale))
 
         self.analysis_denseinception = DenseInceptionBlock(
-            in_channels=256, out_channels=512, depth=12
+            in_channels=int(256 * scale),
+            out_channels=int(512 * scale),
+            depth=int(12 * scale),
         )
-        self.analysis_downsampling4 = DownsamplingBlock(in_channels=512)
+        self.analysis_downsampling4 = DownsamplingBlock(in_channels=int(512 * scale))
 
         # middle block
         self.middle_denseinception = DenseInceptionBlock(
-            in_channels=512, out_channels=1024, depth=24
+            in_channels=int(512 * scale),
+            out_channels=int(1024 * scale),
+            depth=int(24 * scale),
         )
 
         # synthesis path
-        self.synthesis_upsampling1 = UpsamplingBlock(in_channels=1024)
+        self.synthesis_upsampling1 = UpsamplingBlock(in_channels=int(1024 * scale))
         self.synthesis_denseinception = DenseInceptionBlock(
-            in_channels=1024, out_channels=512, depth=12
+            in_channels=int(1024 * scale),
+            out_channels=int(512 * scale),
+            depth=int(12 * scale),
         )
 
-        self.synthesis_upsampling2 = UpsamplingBlock(in_channels=512)
-        self.synthesis_inception2 = InceptionResBlock(in_channels=512, out_channels=256)
-        self.synthesis_upsampling3 = UpsamplingBlock(in_channels=256)
-        self.synthesis_inception3 = InceptionResBlock(in_channels=256, out_channels=128)
-        self.synthesis_upsampling4 = UpsamplingBlock(in_channels=128)
-        self.synthesis_inception4 = InceptionResBlock(in_channels=128, out_channels=64)
+        self.synthesis_upsampling2 = UpsamplingBlock(in_channels=int(512 * scale))
+        self.synthesis_inception2 = InceptionResBlock(
+            in_channels=int(512 * scale), out_channels=int(256 * scale)
+        )
+        self.synthesis_upsampling3 = UpsamplingBlock(in_channels=int(256 * scale))
+        self.synthesis_inception3 = InceptionResBlock(
+            in_channels=int(256 * scale), out_channels=int(128 * scale)
+        )
+        self.synthesis_upsampling4 = UpsamplingBlock(in_channels=int(128 * scale))
+        self.synthesis_inception4 = InceptionResBlock(
+            in_channels=int(128 * scale), out_channels=int(64 * scale)
+        )
 
-        self.synthesis_inception5 = InceptionResBlock(in_channels=64, out_channels=32)
-        self.synthesis_inception6 = InceptionResBlock(in_channels=32, out_channels=2)
+        self.synthesis_inception5 = InceptionResBlock(
+            in_channels=int(64 * scale), out_channels=int(32 * scale)
+        )
+        self.synthesis_inception6 = InceptionResBlock(
+            in_channels=int(32 * scale), out_channels=out_channels
+        )
 
     def forward(self, x):
         x = self.analysis_inception1(x)
--- tests/diunet_test.py
+++ tests/diunet_test.py
@@ -1,13 +1,14 @@
 import torch
 from diunet import DIUNet
+torch.cuda.empty_cache()
 
-model = DIUNet()
-print(torch.cuda.is_available())
+model = DIUNet(out_channels=2, scale = 0.25)
+print(f"CUDA: {torch.cuda.is_available()}")
 print(
     f"Trainable parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}"
 )
 
 test_data = torch.randn((1, 1, 512, 512))
-# with torch.no_grad():
-#     x = model(test_data)
-#     print(x.shape)
+with torch.no_grad():
+    x = model(test_data)
+    print(x.shape)
